28c8b39ddf4177f1d7fe3dde15afc124ccac0798
mfranciszkiewicz <mfranciszkiewicz@gmail.com>
2017-07-05 18:58:30 +0200

    GUI checksum

--- Casks/golem.rb
+++ Casks/golem.rb
@@ -1,6 +1,6 @@
 cask 'golem' do
   version '0.1.0'
-  sha256 'fe5d2a986171ee3cc355c6317653214a5079c73f072b58c9198416a5edb43d3d'
+  sha256 '719a579c61aa53981b2cd2e141bc8ffb30b9ad1c81e3efdf14cce943cafcad7e'
   url "https://github.com/golemfactory/golem-electron/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
 
   name 'Golem Worldwide Supercomputer'
