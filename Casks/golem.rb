e3d58e7803baaeb2bb89d7525ead9c6ccd62e43c
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-12-18 19:14:59 +0100

    revert fixed url

--- Casks/golem.rb
+++ Casks/golem.rb
@@ -1,8 +1,7 @@
 cask 'golem' do
   version '0.1.5'
   sha256 'ca8f453e5c2debfc636c8ed7b0b396a4c778cf3081be8bdbb4208a957fc8eb70'
-  url "https://github.com/golemfactory/golem-electron-dev/releases/download/untagged-9104f4208dbd9831015b/golem-electron-macos-0.1.5.dmg"
-  #url "https://github.com/golemfactory/golem-electron-dev/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
+  url "https://github.com/golemfactory/golem-electron-dev/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
 
   name 'Golem Worldwide Supercomputer'
   homepage 'https://github.com/golemfactory/golem-dev/'
