1bb687889bad2b50bf58d315c1dfc8a12f663dd8
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-12-18 17:48:17 +0100

    Changed versions and SHA's for latest rc

--- golem.rb
+++ golem.rb
@@ -1,9 +1,9 @@
 class Golem < Formula
   desc 'Golem Worldwide Supercomputer'
   homepage 'https://github.com/golemfactory/golem-dev/'
-  url 'https://github.com/golemfactory/golem-dev/releases/download/0.9.1/golem-macos-0.9.1.tar.gz'
-  sha256 'a7e4338c4c18a6ff9711a99342ab0bcbe9f1af47bf4e93bfbcc62678d7bc9e53'
-  version '0.9.1'
+  url 'https://github.com/golemfactory/golem-dev/releases/download/0.10.0/golem-0.10.0-macos.tar.gz'
+  sha256 'fb5814a85f79c6a0d7e036cef0e0cfc5d1a9b84d20e54bf590c90cc3153446fd'
+  version '0.10.0'
 
   depends_on 'docker'
   depends_on 'docker-machine'
