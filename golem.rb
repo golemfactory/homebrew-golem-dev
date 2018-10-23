02c6464a504c1d717e4d019ea64b56ef1f5c8a68
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2018-10-23 14:44:27 +0200

    Version of core & GUI bumped, checksums updated

--- golem.rb
+++ golem.rb
@@ -1,9 +1,9 @@
 class Golem < Formula
   desc 'Golem Worldwide Supercomputer'
   homepage 'https://github.com/golemfactory/golem-dev/'
-  version '0.17.1'
-  sha256 'b95001a299b529aabedf7a58b82b7d666cd924443c1f6a28705a9a21a9dff1f0'
-  url 'https://github.com/golemfactory/golem-dev/releases/download/0.17.1/golem-0.17.1-macOS.tar.gz'
+  version '0.18.0'
+  sha256 '6f353afe5aa1135460db4f2289846e998500c5101663bbdad6d34078d1e2d430'
+  url 'https://github.com/golemfactory/golem-dev/releases/download/0.18.0/golem-0.18.0-macOS.tar.gz'
 
   depends_on 'docker'
   depends_on 'docker-machine'
