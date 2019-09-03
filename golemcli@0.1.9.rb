c4650709de94c42876e257a2a9a723322dc07788
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-09-03 14:17:13 +0200

    Url's updated with cdn alternatives

--- golemcli@0.1.9.rb
+++ golemcli@0.1.9.rb
@@ -4,7 +4,7 @@ class Golemcli < Formula
 
   stable do
     version '0.1.9'
-    url 'https://github.com/golemfactory/golem-client/releases/download/v0.1.9/golemcli-osx-v0.1.9.tar.gz'
+    url 'https://golem-releases.cdn.golem.network/releases/0.20.2/components/golemcli-osx-v0.1.9.tar.gz'
     sha256 '1aa8c3cb9ad3d58ba9baaf594b8425d8d574a8241be6cdd2cf2ed955578ed55d'
   end
 
